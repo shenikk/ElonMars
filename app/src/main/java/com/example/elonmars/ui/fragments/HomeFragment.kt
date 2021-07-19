cc30b042aa4f3038c28033af529c96c6a9bb4c35
Anastasiia Khyshova <a.khyshova@adguard.com>
2021-07-19 16:40:41 +0300

    Replace fragments to the package

--- app/src/main/java/com/example/elonmars/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/elonmars/ui/fragments/HomeFragment.kt
@@ -1,15 +1,16 @@
-package com.example.elonmars
+package com.example.elonmars.ui.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.elonmars.R
 
-class GalleryFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_gallery, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/elonmars/ui/fragments/RoverFragment.kt
+++ app/src/main/java/com/example/elonmars/ui/fragments/RoverFragment.kt
@@ -1,10 +1,11 @@
-package com.example.elonmars
+package com.example.elonmars.ui.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.elonmars.R
 
 class RoverFragment : Fragment() {
 
