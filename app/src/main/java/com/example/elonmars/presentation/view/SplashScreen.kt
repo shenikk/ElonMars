af66b9a59c873015084258e3a0fa9089daeb427f
Anastasiia Khyshova <a.khyshova@adguard.com>
2021-09-14 22:23:16 +0300

    Implement Splash screen

---
+++ app/src/main/java/com/example/elonmars/presentation/view/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.elonmars.presentation.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
+import com.example.elonmars.R
+import kotlin.concurrent.thread
+import android.os.Looper
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
